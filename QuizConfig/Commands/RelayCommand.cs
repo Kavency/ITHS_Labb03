f118265fb91e5f103a1b936f8ef76ab2c782cc99
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-03 00:29:15 +0100

    Add constructor

--- QuizConfig/Commands/RelayCommand.cs
+++ QuizConfig/Commands/RelayCommand.cs
@@ -10,6 +10,15 @@ namespace QuizConfig.Commands
         public event EventHandler? CanExecuteChanged;
 
 
+        public RelayCommand(Action<object?> execute, Func<object?, bool> canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
