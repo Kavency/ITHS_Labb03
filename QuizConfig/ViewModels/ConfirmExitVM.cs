1473bed41cbcc25a904eaed8bfd47aba97a1e9f1
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-03 23:13:05 +0100

    Add Dialog and VM

---
+++ QuizConfig/ViewModels/ConfirmExitVM.cs
@@ -0,0 +1,28 @@
+﻿using QuizConfig.Commands;
+using System.Windows;
+
+namespace QuizConfig.ViewModels
+{
+    internal class ConfirmExitVM : Base
+    {
+        private readonly MainVM _mainVM;
+        public ConfirmExitVM(MainVM mainVM)
+        {
+            this._mainVM = mainVM;
+            ExitProgramCMD = new RelayCommand(Answer);
+        }
+
+        public RelayCommand ExitProgramCMD { get; }
+
+        private void Answer(object? obj)
+        {
+            if (obj is Window window)
+                window.Close();
+            else
+            {
+                // TODO: Save to File
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
