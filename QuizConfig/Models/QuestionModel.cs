0977945222422be3f3fe6e8948191dc29994651b
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-03 15:21:21 +0100

    Complete questionModel

--- QuizConfig/Models/QuestionModel.cs
+++ QuizConfig/Models/QuestionModel.cs
@@ -1,7 +1,49 @@
-﻿namespace QuizConfig.Models
+﻿using System.Text.Json.Serialization;
+
+namespace QuizConfig.Models
 {
     internal class QuestionModel
     {
+        #region Properties
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+
+        [JsonPropertyName("difficulty")]
+        public string Difficulty { get; set; }
+
+
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+
+
+        [JsonPropertyName("question")]
         public string Question { get; set; }
+
+
+        [JsonPropertyName("correct_answer")]
+        public string CorrectAnswer { get; set; }
+
+
+        [JsonPropertyName("incorrect_answers")]
+        public string[] IncorrectAnswers { get; set; }
+        #endregion
+
+
+        #region Constructors
+        public QuestionModel() { }
+
+        public QuestionModel(string question, string correctAnswer, string incorrectAnswer1,
+            string incorrectAnswer2, string incorrectAnswer3, string type = "Multiple",
+            string difficulty = "Medium", string category = "General")
+        {
+            this.Question = question;
+            this.CorrectAnswer = correctAnswer;
+            this.IncorrectAnswers = [incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
+            this.Type = type;
+            this.Difficulty = difficulty;
+            this.Category = category;
+        }
+        #endregion
     }
 }
