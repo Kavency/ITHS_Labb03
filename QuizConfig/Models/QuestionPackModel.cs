2d7ae8ca205d27b0d350fa1195ae297dc98563a6
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-03 15:30:16 +0100

    Complete QuestionPackModel

--- QuizConfig/Models/QuestionPackModel.cs
+++ QuizConfig/Models/QuestionPackModel.cs
@@ -4,12 +4,25 @@ namespace QuizConfig.Models
 {
     internal class QuestionPackModel
     {
+        #region Properties
         public string Name { get; set; }
+        public int TimeLimit { get; set; }
+        public string Difficulty { get; set; }
         public ObservableCollection<QuestionModel> Questions { get; set; }
+        #endregion
 
-        public QuestionPackModel(QuestionModel questionModel)
+
+        #region Constructors
+        public QuestionPackModel() { }
+
+        public QuestionPackModel(string name, int timeLimit = 30, string difficulty = "Medium")
         {
             this.Questions = new ObservableCollection<QuestionModel>();
+
+            this.Name = name;
+            this.TimeLimit = timeLimit;
+            this.Difficulty = difficulty;
         }
+        #endregion
     }
 }
