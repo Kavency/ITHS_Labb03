5241429ca2aa79e79072a629c48292007206eb31
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-04 09:25:29 +0100

    Refactor

--- QuizConfig/ViewModels/QuestionVM.cs
+++ QuizConfig/ViewModels/QuestionVM.cs
@@ -6,14 +6,13 @@ namespace QuizConfig.ViewModels
     {
         private readonly QuestionModel _questionModel;
 
-        #region Properties
         public string Type => _questionModel.Type;
         public string Difficulty => _questionModel.Difficulty;
         public string Category => _questionModel.Category;
         public string Question => _questionModel.Question;
         public string CorrectAnswer => _questionModel.CorrectAnswer;
         public string[] IncorrectAnswers => _questionModel.IncorrectAnswers;
-        #endregion
+
 
         public QuestionVM(QuestionModel questionModel)
         {
