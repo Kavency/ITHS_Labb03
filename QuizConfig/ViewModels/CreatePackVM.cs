ab58cfa0e1220e0fba4ce33c236b47c7bcb69b84
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-04 23:04:23 +0100

    Add ViewModels

---
+++ QuizConfig/ViewModels/CreatePackVM.cs
@@ -0,0 +1,41 @@
+﻿using QuizConfig.Commands;
+using QuizConfig.Models;
+using QuizConfig.Views.Dialogs;
+using System.Windows;
+
+namespace QuizConfig.ViewModels
+{
+    internal class CreatePackVM : BaseVM
+    {
+        public MainVM MainVM { get; }
+
+        public string Name { get; set; }
+        public string Difficulty { get; set; } = "Medium";
+        public int TimeLimit { get; set; } = 30;
+
+        public RelayCommand CreateCMD { get; }
+        public RelayCommand CancelCMD { get; }
+
+        public CreatePackVM(MainVM mainVM)
+        {
+            this.MainVM = mainVM;
+
+            this.CreateCMD = new RelayCommand(Create);
+            this.CancelCMD = new RelayCommand(Cancel);
+        }
+
+
+        private async void Create(object? obj)
+        {
+            QuestionPackVM newPack = new QuestionPackVM(new QuestionPackModel(Name, TimeLimit, Difficulty));
+            MainVM.QuestionPacks.Add(newPack);
+            MainVM.ActivePack = newPack;
+            await MainVM.FileHandler.SaveToFileAsync();
+            Cancel(obj);
+        }
+        private void Cancel(object? obj)
+        {
+            Application.Current.Windows.OfType<CreateNewPackDialog>().FirstOrDefault()?.Close();
+        }
+    }
+}
