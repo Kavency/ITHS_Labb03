29c8d78b47c4968404126b7419946ed151d97704
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-04 09:03:55 +0100

    Add VM

---
+++ QuizConfig/ViewModels/QuestionVM.cs
@@ -0,0 +1,23 @@
+﻿using QuizConfig.Models;
+
+namespace QuizConfig.ViewModels
+{
+    internal class QuestionVM : BaseVM
+    {
+        private readonly QuestionModel _questionModel;
+
+        #region Properties
+        public string Type => _questionModel.Type;
+        public string Difficulty => _questionModel.Difficulty;
+        public string Category => _questionModel.Category;
+        public string Question => _questionModel.Question;
+        public string CorrectAnswer => _questionModel.CorrectAnswer;
+        public string[] IncorrectAnswers => _questionModel.IncorrectAnswers;
+        #endregion
+
+        public QuestionVM(QuestionModel questionModel)
+        {
+            this._questionModel = questionModel;
+        }
+    }
+}
