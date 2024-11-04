f3e27616170f5bed0d7451771b1d332ec7ad4e14
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-04 09:22:39 +0100

    Add VM

---
+++ QuizConfig/ViewModels/QuestionPackVM.cs
@@ -0,0 +1,21 @@
+﻿using QuizConfig.Models;
+using System.Collections.ObjectModel;
+
+namespace QuizConfig.ViewModels
+{
+    internal class QuestionPackVM
+    {
+        private readonly QuestionPackModel _questionPackModel;
+
+        public string Name => _questionPackModel.Name;
+        public int TimeLimit => _questionPackModel.TimeLimit;
+        public string Difficulty => _questionPackModel.Difficulty;
+        public ObservableCollection<QuestionModel> Questions => _questionPackModel.Questions;
+
+
+        public QuestionPackVM(QuestionPackModel questionPackModel)
+        {
+            this._questionPackModel = questionPackModel;
+        }
+    }
+}
