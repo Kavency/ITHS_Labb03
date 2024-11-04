b9e8fc42179fb8949b28f0bc90457b8bcb0329c0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-04 23:06:10 +0100

    Add dialogs

---
+++ QuizConfig/Views/Dialogs/CreateNewPackDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace QuizConfig.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for CreateNewPackDialog.xaml
+    /// </summary>
+    public partial class CreateNewPackDialog : Window
+    {
+        public CreateNewPackDialog()
+        {
+            InitializeComponent();
+            DataContext = ((MainWindow)Application.Current.MainWindow).DataContext;
+        }
+    }
+}
---
+++ QuizConfig/Views/Dialogs/OptionsDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace QuizConfig.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for OptionsDialog.xaml
+    /// </summary>
+    public partial class OptionsDialog : Window
+    {
+        public OptionsDialog()
+        {
+            InitializeComponent();
+            DataContext = ((MainWindow)Application.Current.MainWindow).DataContext;
+        }
+    }
+}
