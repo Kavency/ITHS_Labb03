611ad77afd233612c4127eda1075ce355eb22b51
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-05 10:11:19 +0100

    Add props and fields

--- QuizConfig/ViewModels/PlayVM.cs
+++ QuizConfig/ViewModels/PlayVM.cs
@@ -1,8 +1,20 @@
-﻿namespace QuizConfig.ViewModels
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace QuizConfig.ViewModels
 {
     internal class PlayVM
     {
+        private DispatcherTimer _timer;
+        private int _timeLimit;
+        private int _elapsedTime;
+
         public MainVM MainVM { get; set; }
+        public QuestionPackVM QuizQestions { get => MainVM.ActivePack; private set { } }
+        public QuestionVM CurrentQuestion { get; set; }
+        public Visibility ResultVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility QuizVisibility { get; set; } = Visibility.Visible;
+
 
         public PlayVM(MainVM mainVM)
         {
