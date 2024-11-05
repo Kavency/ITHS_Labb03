7f11e51799b44e7dbe593aebbaab979065082dfc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-05 09:18:11 +0100

    Add ViewModel

---
+++ QuizConfig/ViewModels/PlayVM.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizConfig.ViewModels
+{
+    internal class PlayVM
+    {
+        public MainVM MainVM { get; set; }
+
+        public PlayVM(MainVM mainVM)
+        {
+            this.MainVM = mainVM;
+        }
+    }
+}
