93a5e6b274b72205898eeb01474cb6f580a7636f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-06 20:38:07 +0100

    Add window position

--- QuizConfig/Views/Windows/MainView.xaml.cs
+++ QuizConfig/Views/Windows/MainView.xaml.cs
@@ -8,9 +8,19 @@ namespace QuizConfig
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double StartupWidth { get; set; }
+        public double StartupHeight { get; set; }
+        public double StartupTop { get; set; }
+        public double StartupLeft { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            StartupWidth = this.Width;
+            StartupHeight = this.Height;
+            StartupTop = this.Top;
+            StartupLeft = this.Left;
         }
     }
 }
