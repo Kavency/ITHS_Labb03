b60f20b58afff5916947e5a491591a82f3fe1690
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-11-06 16:54:15 +0100

    Add a custom binder.

---
+++ QuizConfig/MiscClasses/EnumBinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Markup;
+
+namespace QuizConfig.MiscClasses
+{
+    public class EnumBinder : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBinder(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+                throw new Exception("Argument type is not an Enum");
+
+            this.EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
